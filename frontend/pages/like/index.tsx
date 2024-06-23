b789552b6ccfbdbbd79029cb1298af18c2d4fa38
bmoll123 <91171193+bmoll123@users.noreply.github.com>
2024-06-23 19:59:13 +0800

    complete post&like

--- frontend/pages/like/index.tsx
+++ frontend/pages/like/index.tsx
@@ -4,6 +4,7 @@ import { Button, ButtonGroup } from "@nextui-org/react";
 import { Select, SelectItem } from "@nextui-org/react";
 import { useRouter } from 'next/router';
 import { LookupIcon, DeleteIcon } from "@/components/icons";
+import { useAuth } from "@/components/hooks/useAuth";
 
 export default function LikePage() {
   return (
@@ -13,13 +14,7 @@ export default function LikePage() {
   );
 }
 
-async function fetchInfo() {
-  const token = localStorage.getItem('RENT_USER');
-  console.log("like token:", token);
-  if (!token) {
-    throw new Error('No token found');
-  }
-
+async function fetchInfo(token: string) {
   // 发起 fetch 请求，并在请求头中包含 JWT 令牌
   const response = await fetch(`${process.env.API_URL}/likes`, {
     method: "GET",
@@ -35,21 +30,35 @@ async function fetchInfo() {
   }
 }
 
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const month = date.getMonth() + 1; // getMonth 返回的是 0 到 11，所以要加 1
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${year}/${month}/${day}`;
+}
+
 function LikeForm() {
   const [likes, setLikes] = useState([]);
-  const [sortOption, setSortOption] = useState('dateDecrease');
+  const [sortOption, setSortOption] = useState('dateIncrease');
+  const { user } = useAuth();
+  const token = user?.token || "no_token";
+  console.log(token);
 
   useEffect(() => {
     async function getData() {
       try {
-        const data = await fetchInfo();
-        setLikes(data.likes);
+        const data = await fetchInfo(token);
+        setLikes(data.liked_rentals);
       } catch (err) {
         console.error("Error fetching likes: ", err);
       }
     }
-    getData();
-  }, []);
+    if (token) {
+      getData();
+    }
+  }, [token]);
 
   useEffect(() => {
     sortBy(sortOption);
@@ -74,45 +83,41 @@ function LikeForm() {
     setLikes(sortedLikes); // Update state with sorted array
   };
 
-  const removeLike = async (id: any, index: number) => {
-    const confirmed = window.confirm(`確定刪除房屋 ${index} ？`);
+  const removeFromFavorites = async (likeToRemove) => {
+    const confirmed = window.confirm(`確定刪除房屋「${likeToRemove.Title}」？`);
     if (!confirmed) {
       return;
     }
 
-    const token = localStorage.getItem('jwt');
-    if (!token) {
-      throw new Error('No token found');
-    }
-
     try {
-      const response = await fetch(`${process.env.API_URL}/rental/${id}`, {
+      const response = await fetch(`${process.env.API_URL}/like`, {
         method: "DELETE",
         headers: {
           'Authorization': `Bearer ${token}`,
           'Content-Type': 'application/json'
-        }
+        },
+        body: JSON.stringify({ rental_id: likeToRemove.R_id })
       });
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      const data = await fetchInfo();
-      setLikes(data.likes);
+      const data = await fetchInfo(token);
+      setLikes(data.liked_rentals);
     } catch (error) {
       console.error('Delete request failed:', error);
     }
   };
 
   const router = useRouter();
-  const handleEdit = (id: any) => {
-    router.push(`/edit/${id}`);
+  const handleLookup = (id: any) => {
+    router.push(`/rental/${id}`);
   };
 
 
   return (
     <div>
       <div className="w-64 mb-5 ml-4">
-        <Select label="選擇排序項目" placeholder="上架日期 新到舊" className="max-w-xs" value={sortOption} onChange={(sortOption) => sortBy(sortOption.target.value)}>
+        <Select label="選擇排序項目" placeholder="上架日期 舊到新" className="max-w-xs" value={sortOption} onChange={(sortOption) => sortBy(sortOption.target.value)}>
           <SelectItem value="dateDecrease" key="dateDecrease">上架日期 新到舊</SelectItem>
           <SelectItem value="dateIncrease" key="dateIncrease">上架日期 舊到新</SelectItem>
           <SelectItem value="priceDecrease" key="priceDecrease">價格 高到低</SelectItem>
@@ -122,20 +127,23 @@ function LikeForm() {
         </Select>
       </div>
       <section className="favorites-list flex flex-wrap">
-        {likes.map((fav) => (
-          <div key={fav.L_id} className="favorite-item border p-5 m-4 rounded-lg basis-64">
+        {likes.map((like) => (
+          <div key={like.R_id} className="favorite-item border p-5 m-4 rounded-lg basis-64">
             <div className="info text-left w-full mt-4">
-              <h2 className="text-lg font-semibold">{fav.name}</h2>
-              <p>地址：{fav.Address}</p>
-              <p>價格：{fav.Price}元/月</p>
-              <p>面積：{fav.Ping}平方公尺</p>
-              <p>房型：{fav.rooms}</p>
-              <p>上架日期：{fav.date}</p>
+              <h2 className="text-lg font-semibold">{like.Title}</h2>
+              <p>地址：{like.Address}</p>
+              <p>價格: {parseInt(like.Price)}/月</p>
+              <p>類型: {like.Type}</p>
+              <p>臥室數量: {like.Bedroom}</p>
+              <p>客廳數量: {like.LivingRoom}</p>
+              <p>浴室數量: {like.Bathroom}</p>
+              <p>坪數: {parseInt(like.Ping)}</p>
+              <p>上架日期: {formatDate(like.PostDate)}</p>
             </div>
             <div className="actions mt-2 ">
               <ButtonGroup>
-                <Button startContent={<LookupIcon />}>查看</Button>
-                <Button color="danger" variant="bordered" onClick={() => removeFromFavorites(fav.L_id)} startContent={<DeleteIcon />}>刪除</Button>
+                <Button onClick={() => handleLookup(like.R_id)} startContent={<LookupIcon />}>查看</Button>
+                <Button color="danger" variant="bordered" onClick={() => removeFromFavorites(like)} startContent={<DeleteIcon />}>刪除</Button>
               </ButtonGroup>
             </div>
           </div>
--- frontend/pages/post/index.tsx
+++ frontend/pages/post/index.tsx
@@ -4,6 +4,7 @@ import { Button, ButtonGroup } from "@nextui-org/react";
 import { Select, SelectItem } from "@nextui-org/react";
 import { useRouter } from 'next/router';
 import { EditIcon, DeleteIcon } from "@/components/icons";
+import { useAuth } from "@/components/hooks/useAuth";
 
 export default function PostPage() {
   return (
@@ -13,13 +14,7 @@ export default function PostPage() {
   );
 }
 
-async function fetchInfo() {
-  const token = localStorage.getItem('RENT_USER.token');
-  console.log("post token:", token);
-  if (!token) {
-    throw new Error('No token found');
-  }
-
+async function fetchInfo(token: string) {
   // 发起 fetch 请求，并在请求头中包含 JWT 令牌
   const response = await fetch(`${process.env.API_URL}/rental`, {
     method: "GET",
@@ -28,28 +23,41 @@ async function fetchInfo() {
     }
   });
   if (!response.ok) {
-    throw new Error(`Error`);
+    throw new Error("Error");
   } else {
     const data = await response.json();
     return data;
   }
 }
 
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const month = date.getMonth() + 1; // getMonth 返回的是 0 到 11，所以要加 1
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${year}/${month}/${day}`;
+}
+
 function PostForm() {
   const [rentals, setRentals] = useState([]);
-  const [sortOption, setSortOption] = useState('dateDecrease');
+  const [sortOption, setSortOption] = useState('dateIncrease');
+  const { user } = useAuth();
+  const token = user?.token || "no_token";
 
   useEffect(() => {
     async function getData() {
       try {
-        const data = await fetchInfo();
+        const data = await fetchInfo(token);
         setRentals(data.rentals);
       } catch (err) {
         console.error("Error fetching rentals: ", err);
       }
     }
-    getData();
-  }, []);
+    if (token) {
+      getData();
+    }
+  }, [token]);
 
   useEffect(() => {
     sortBy(sortOption);
@@ -58,35 +66,39 @@ function PostForm() {
   const sortBy = (option: string) => {
     setSortOption(option);
     let sortedRentals = [...rentals]; // Create a copy of state
-    if (option === "dateDecrease") {
-      sortedRentals.sort((a, b) => new Date(b.PostDate) - new Date(a.PostDate));
-    } else if (option === "dateIncrease") {
-      sortedRentals.sort((a, b) => new Date(a.PostDate) - new Date(b.PostDate));
-    } else if (option === "priceDecrease") {
-      sortedRentals.sort((a, b) => parseInt(b.Price) - parseInt(a.Price));
-    } else if (option === "priceIncrease") {
-      sortedRentals.sort((a, b) => parseInt(a.Price) - parseInt(b.Price));
-    } else if (option === "sizeDecrease") {
-      sortedRentals.sort((a, b) => parseInt(b.Ping) - parseInt(a.Ping));
-    } else if (option === "sizeIncrease") {
-      sortedRentals.sort((a, b) => parseInt(a.Ping) - parseInt(b.Ping));
+    switch (option) {
+      case "dateDecrease":
+        sortedRentals.sort((a, b) => new Date(b.PostDate).getTime() - new Date(a.PostDate).getTime());
+        break;
+      case "dateIncrease":
+        sortedRentals.sort((a, b) => new Date(a.PostDate).getTime() - new Date(b.PostDate).getTime());
+        break;
+      case "priceDecrease":
+        sortedRentals.sort((a, b) => parseInt(b.Price) - parseInt(a.Price));
+        break;
+      case "priceIncrease":
+        sortedRentals.sort((a, b) => parseInt(a.Price) - parseInt(b.Price));
+        break;
+      case "sizeDecrease":
+        sortedRentals.sort((a, b) => parseInt(b.Ping) - parseInt(a.Ping));
+        break;
+      case "sizeIncrease":
+        sortedRentals.sort((a, b) => parseInt(a.Ping) - parseInt(b.Ping));
+        break;
+      default:
+        break;
     }
     setRentals(sortedRentals); // Update state with sorted array
   };
 
-  const removePost = async (id: any, index: number) => {
-    const confirmed = window.confirm(`確定刪除房屋 ${index} ？`);
+  const removePost = async (rentalToRemove) => {
+    const confirmed = window.confirm(`確定刪除房屋「${rentalToRemove.Title}」？`);
     if (!confirmed) {
       return;
     }
 
-    const token = localStorage.getItem('jwt');
-    if (!token) {
-      throw new Error('No token found');
-    }
-
     try {
-      const response = await fetch(`${process.env.API_URL}/rental/${id}`, {
+      const response = await fetch(`${process.env.API_URL}/rental/${rentalToRemove.R_id}`, {
         method: "DELETE",
         headers: {
           'Authorization': `Bearer ${token}`,
@@ -96,7 +108,7 @@ function PostForm() {
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      const data = await fetchInfo();
+      const data = await fetchInfo(token);
       setRentals(data.rentals);
     } catch (error) {
       console.error('Delete request failed:', error);
@@ -111,7 +123,7 @@ function PostForm() {
   return (
     <div>
       <div className="w-64 mb-5 ml-4">
-        <Select label="選擇排序項目" placeholder="發布日期 新到舊" className="max-w-xs" value={sortOption} onChange={(sortOption) => sortBy(sortOption.target.value)}>
+        <Select label="選擇排序項目" placeholder="發布日期 舊到新" className="max-w-xs" value={sortOption} onChange={(sortOption) => sortBy(sortOption.target.value)}>
           <SelectItem value="dateDecrease" key="dateDecrease">發佈日期 新到舊</SelectItem>
           <SelectItem value="dateIncrease" key="dateIncrease">發佈日期 舊到新</SelectItem>
           <SelectItem value="priceDecrease" key="priceDecrease">價格 高到低</SelectItem>
@@ -121,22 +133,23 @@ function PostForm() {
         </Select>
       </div>
       <section className="favorites-list flex flex-wrap">
-        {rentals.map((rental, index) => (
+        {rentals.map((rental) => (
           <div key={rental.R_id} className="favorite-item border p-5 m-4 rounded-lg basis-64">
             <div className="info text-left w-full mt-4">
-              <h2 className="text-lg font-semibold">房屋：{index + 1}</h2>
+              <h2 className="text-lg font-semibold">{rental.Title}</h2>
               <p>地址: {rental.Address}</p>
-              <p>價格: {parseInt(rental.Price)}</p>
+              <p>價格: {parseInt(rental.Price)}/月</p>
               <p>類型: {rental.Type}</p>
               <p>臥室數量: {rental.Bedroom}</p>
               <p>客廳數量: {rental.LivingRoom}</p>
               <p>浴室數量: {rental.Bathroom}</p>
               <p>坪數: {parseInt(rental.Ping)}</p>
+              <p>上架日期: {formatDate(rental.PostDate)}</p>
             </div>
             <div className="actions mt-2 ">
               <ButtonGroup>
                 <Button onClick={() => handleEdit(rental.R_id)} startContent={<EditIcon />}>編輯</Button>
-                <Button color="danger" variant="bordered" onClick={() => removePost(rental.R_id, index + 1)} startContent={<DeleteIcon />}>刪除</Button>
+                <Button color="danger" variant="bordered" onClick={() => removePost(rental)} startContent={<DeleteIcon />}>刪除</Button>
               </ButtonGroup>
             </div>
           </div>
